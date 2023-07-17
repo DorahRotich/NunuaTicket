01776ea3cd8cf35e924768d721fbc36422dd2176
palvin254 <palvinpaul900m@gmail.com>
2023-07-17 16:34:42 +0300

    palvin

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -2,8 +2,14 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
+
+  const {data, loading, error } = useFetch("http://localhost:8080")
   return (
     <div className="featured">
+      {loading ? (
+        "loading please wait"
+      ) : (
+      <>
       <div className="featuredItem">
         <img
           src="https://i.pinimg.com/474x/f7/b8/ee/f7b8ee522de61a8d05ee200b1c077d81.jpg"
@@ -12,7 +18,7 @@ const Featured = () => {
         />
         <div className="featuredTitles">
           <h1>Nairobi</h1>
-          <h2>3events</h2>
+          <h2>{data[0]}</h2>
         </div>
       </div>
       
@@ -24,20 +30,22 @@ const Featured = () => {
         />
         <div className="featuredTitles">
           <h1>Kajiado</h1>
-          <h2> 4events</h2>
+          <h2>{data[1]}</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img
           src="https://i.pinimg.com/474x/a1/d4/cb/a1d4cb9f2fe8c724860e14d8014ed620.jpg"
           alt="Thika city"
-          className="featuredImg"
+          className="featuredImg" 
         />
         <div className="featuredTitles">
           <h1>Thika town</h1>
-          <h2>5events</h2>
+          <h2>{data[2]}</h2>
         </div>
       </div>
+      </>
+      )}
     </div>
   );
 };
