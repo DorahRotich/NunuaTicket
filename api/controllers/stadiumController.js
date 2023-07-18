0188dec78a54cfc83ebca09380a0cb658758074d
DorahRotich <rotichkurgat@gmail.com>
2023-07-18 17:01:36 +0300

    add count by type

--- api/controllers/stadiumController.js
+++ api/controllers/stadiumController.js
@@ -1,3 +1,4 @@
+import List from '../../client/src/pages/list/List.jsx'
 import Stadium from '../models/stadiumModel.js'
 
 export const createStadium = async (req, res, next) => {
@@ -61,14 +62,22 @@ export const countByCity = async (req, res, next) => {
         next(error)
     }
 };
-export const countByType = async (req, res, next) => {
-    const cities = req.query.cities.split(",");
-   
+export const countbyType = async (req, res, next) => {
     try{
-        const citiesList = await Promise.all(cities.map((city) => {
-            return Stadium.countDocuments({ city:city })
-        }))
-        res.status(200).json(citiesList)
+    const EPLCount = await Stadium.countDocuments({ type: "EPL" });
+    const CountyCount = await Stadium.countDocuments({ type: "County" });
+    const concertCount = await Stadium.countDocuments({ type: "concert" });
+    const rallyCount = await  Stadium.countDocuments({ type: "rally" });
+    const sportsCount = await Stadium.countDocuments({ type: "sports" });
+   
+    
+    res.status(200).json([
+        { type: "EPL", count: EPLCount },
+        { type: "County", count: CountyCount },
+        { type: "concert", count: concertCount },
+        { type: "rally", count: rallyCount },
+        { type: "sports", count: sportsCount },  
+    ]);
     } catch (error) {
         next(error)
     }
