49e0d34c94c0f24e095c4d3ca36bd354d77d9895
Palvin254 <palvinpaul900m@gmail.com>
2023-07-19 00:43:02 +0300

    updated routers

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -41,7 +41,7 @@ const Header = ({ type }) => {
   };
 
   const handleSearch = () => {
-    navigate("/hotels", { state: { destination, date, options } });
+    navigate("/stadium", { state: { destination, date, options } });
   };
 
   return (
