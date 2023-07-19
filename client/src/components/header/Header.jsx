8275b2c37a85edb0347db3e5ea564b3c11ee99ce
Palvin254 <palvinpaul900m@gmail.com>
2023-07-19 22:48:03 +0300

    signin/register button

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,16 +6,18 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 import { DateRange } from "react-date-range";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext"
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
   const [openDate, setOpenDate] = useState(false);
-  const [date, setDate] = useState([
+  const [dates, setDate] = useState([
     {
       startDate: new Date(),
       endDate: new Date(),
@@ -26,10 +28,11 @@ const Header = ({ type }) => {
   const [options, setOptions] = useState({
     adult: 1,
     children: 0,
-    room: 1,
+    seat: 1,
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -40,8 +43,17 @@ const Header = ({ type }) => {
     });
   };
 
+  const handleClick = () => {
+
+      navigate("/login");
+  
+  };
+
+  const { dispatch } = useContext(SearchContext);
+
   const handleSearch = () => {
-    navigate("/stadium", { state: { destination, date, options } });
+    dispatch({type:"NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/stadium", { state: { destination, dates, options } });
   };
 
   return (
@@ -61,7 +73,7 @@ const Header = ({ type }) => {
               Get rewarded for attending events – unlock instant savings of 10% or
               more with a free NunuaTicket account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button onClick={handleClick} className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faLocationDot} className="headerIcon" />
@@ -77,8 +89,8 @@ const Header = ({ type }) => {
                 <span
                   onClick={() => setOpenDate(!openDate)}
                   className="headerSearchText"
-                >{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                  date[0].endDate,
+                >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                  dates[0].endDate,
                   "MM/dd/yyyy"
                 )}`}</span>
                 {openDate && (
@@ -86,7 +98,7 @@ const Header = ({ type }) => {
                     editableDateInputs={true}
                     onChange={(item) => setDate([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={date}
+                    ranges={dates}
                     className="date"
                     minDate={new Date()}
                   />
@@ -97,7 +109,7 @@ const Header = ({ type }) => {
                 <span
                   onClick={() => setOpenOptions(!openOptions)}
                   className="headerSearchText"
-                >{`${options.adult} adult · ${options.children} children · ${options.room} room`}</span>
+                >{`${options.adult} adult · ${options.children} children · ${options.seat} seat`}</span>
                 {openOptions && (
                   <div className="options">
                     <div className="optionItem">
@@ -146,14 +158,14 @@ const Header = ({ type }) => {
                       <span className="optionText">Seat</span>
                       <div className="optionCounter">
                         <button
-                          disabled={options.room <= 1}
+                          disabled={options.seat <= 1}
                           className="optionCounterButton"
                           onClick={() => handleOption("seat", "d")}
                         >
                           -
                         </button>
                         <span className="optionCounterNumber">
-                          {options.room}
+                          {options.seat}
                         </span>
                         <button
                           className="optionCounterButton"
