21898b05b456484613793ce57ca0609d24154a07
Palvin254 <palvinpaul900m@gmail.com>
2023-07-19 22:39:54 +0300

    Updated List file

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -11,17 +11,17 @@ import useFetch from "../../hooks/useFetch";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
-  const [date, setDate] = useState(location.state.date);
+  const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
   const [min, setMin] = useState(undefined);
   const [max, setMax] = useState(undefined);
 
   const {data, loading, error, reFetch } = useFetch(
-    `http://localhost:8080/api/stadium?min=${min || 0 }&max=${max || 999}${destination? `&city=${destination}`:""}`
+   // `stadium?city=${destination}&min=${min || 0 }&max=${max || 999}`
+   `http://localhost:8080/api/stadium?min=${min || 0 }&max=${max || 999}${destination? `&city=${destination}`:""}`
     
-  )
-  console.log(data)
+  );
 
   const handleClick = () => {
     reFetch();
@@ -42,14 +42,14 @@ const List = () => {
             <div className="lsItem">
               <label> Event Date</label>
               <span onClick={() => setOpenDate(!openDate)}>{`${format(
-                date[0].startDate,
+                dates[0].startDate,
                 "MM/dd/yyyy"
-              )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+              )} to ${format(dates[0].endDate, "MM/dd/yyyy")}`}</span>
               {openDate && (
                 <DateRange
-                  onChange={(item) => setDate([item.selection])}
+                  onChange={(item) => setDates([item.selection])}
                   minDate={new Date()}
-                  ranges={date}
+                  ranges={dates}
                 />
               )}
             </div>
@@ -109,7 +109,7 @@ const List = () => {
           </div>
           <div className="listResult">
           {loading ? (
-              "loading please wait"
+              "loading"
             ) : (
               <>
                 {data.map((item) => (
